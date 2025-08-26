e97094622901169f74cde50d61cc06224612fa80
andreim-udc <andreim@utah.gov>
2025-08-26 13:43:53 -0600

    Auto-generated commit

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,17 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
-importScripts('./ngsw-worker.js');
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
+);
+importScripts("./ngsw-worker.js");
 
-self.addEventListener('install', (event) => {
-  console.log('Service worker installed');
+self.addEventListener("install", (event) => {
+  console.log("Service worker installed");
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('Service worker activated');
+self.addEventListener("activate", (event) => {
+  console.log("Service worker activated");
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
@@ -17,33 +19,32 @@ self.addEventListener('fetch', (event) => {
   );
 });
 
-const CACHE_NAME = 'sup-contact-cache-v1';
+const CACHE_NAME = "ut-udc-apnp-field_contact-cache-v1";
 const urlsToCache = [
-    '/index.html',
-    // './offline.html',
-    // './favicon.ico',
-    // './icons/*',
-    // './assets/icons/*',
-    // './assets/images/*',
-    // './assets/data/*'
-]
+  "/index.html",
+  // './offline.html',
+  // './favicon.ico',
+  // './icons/*',
+  // './assets/icons/*',
+  // './assets/images/*',
+  // './assets/data/*'
+];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      fetch(event.request)
-        .catch(() => caches.match(event.request)
-          .then(response => {
-            return response || caches.match('/offline.html');
-          }))
-    );
-  });
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request).then((response) => {
+        return response || caches.match("/offline.html");
+      })
+    )
+  );
+});
