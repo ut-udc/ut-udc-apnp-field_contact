7ba24d4bfee8aba3e13a29eea663644c783fbe5c
andreim-udc <andreim@utah.gov>
2025-10-02 13:02:48 -0600

    Added agent profile

---
+++ src/app/components/agent-profile/agent-profile.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-agent-profile',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './agent-profile.html',
+  styleUrl: './agent-profile.scss'
+})
+export class AgentProfile {
+
+}
